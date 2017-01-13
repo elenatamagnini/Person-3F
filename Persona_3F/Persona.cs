5324f012fe5ee726592eedde25f96c03fb10fe3e
Elena <by-ele@hotmail.it>
2017-01-13 21:04:47 +0100

    Creazione classe Persona e classe figlia Studente

---
+++ Persona_3F/Persona.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persona_3F
+{
+    class Persona
+    {
+        public Persona(string nome, string cognome, int anni)
+            {
+
+            }
+
+        public string nome { get; set; }
+
+        public string cognome { get; set; }
+
+        private int _anni;
+        public int anni
+        {
+            set
+            {
+                if (value < 0) value = 1;
+                _anni = value;
+            }
+            get
+            {
+                return _anni;
+            }
+        }
+
+        private string _codicefiscale;
+        public string codicefiscale { get { return nome + cognome + anni; } }
+
+        public string describe()
+        {
+            string output = "Nome: " + nome + "\r\n";
+            output += "Cognome: " + cognome + "\r\n";
+            output += "Anni: " + anni + "\r\n";
+
+            return output;
+        }
+    }
+}
---
+++ Persona_3F/Studente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persona_3F
+{
+    class Studente : Persona
+    {
+        public Studente(string nome, string cognome, int anni, string sezione) : base(nome, cognome, anni)
+            {
+
+            }
+
+        public string sezione { get; set; }
+
+    }
+}

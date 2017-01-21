7fd4ac6373a5330c297d7af108787f004f9e3c08
Elena <by-ele@hotmail.it>
2017-01-21 18:21:00 +0100

    Creazione array

--- Persona_3F/Form1.cs
+++ Persona_3F/Form1.cs
@@ -12,9 +12,25 @@ namespace Persona_3F
 {
     public partial class Form1 : Form
     {
+        private string[] nomi = { "Tizio", "Caio", "Sempronio" };
+        private string[] cognomi = { "Rossi", "Bianchi", "Verdi" };
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Console.WriteLine(generateRandomPersona());
+            Console.WriteLine(generateRandomPersona());
+            Console.WriteLine(generateRandomPersona());
+        }
+
+        Random rnd = new Random();
+        private Persona generateRandomPersona()
+        {
+            return new Persona(nomi[rnd.Next(0, 3)], cognomi[rnd.Next(0, 3)], rnd.Next(18, 50));
         }
     }
 }
